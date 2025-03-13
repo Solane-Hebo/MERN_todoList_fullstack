3232c88af05450674c31e237b567d9f5e635fd91
Solane Hebo <solanesani1@gmail.com>
2025-03-13 15:17:59 +0100

    Added controlers

---
+++ backend_todoListDB/src/controlers/todo.controlers.js
@@ -0,0 +1,83 @@
+import mongoose from 'mongoose';
+import Todos from "../models/todos.models.js";
+import asyncHandler from "express-async-handler"
+
+// CRUD
+// Create
+export const createTodoList = asyncHandler(async(req, res, next) =>{
+    const { title, content } = req.body
+
+
+    if(!title && !content) {
+        return res.status(400).json({message: "Title and content are required"})
+    }
+
+    const todoList = await Todos.create({title, content})
+    res.status(200).json(todoList)
+})
+
+// ReadAll
+export const getTodoList = asyncHandler(async(req, res, next) => {
+    const todoList = await Todos.find().exec()
+    res.status(200).json(todoList)
+})
+
+// ReadOne
+export const getTodoListById = asyncHandler(async(req, res, next) =>{
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({message: "Invalid Id"})
+    }
+
+    const todoList = await Todos.findById(id).exec()
+
+    if(!todoList){
+        return res.status(400).json({message: "TodoList not found"})
+    }
+
+    res.status(200).json(todoList)
+})
+
+// Update
+export const updateTodoList = asyncHandler( async(req, res, next) => {
+    const {id} = req.params
+    const {title, content} = req.body
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({message: "Invalid Id"})
+}
+ 
+ const toUpdate = {}
+ if(title) toUpdate.title = title
+ if(content) toUpdate.content = content
+
+ if(Object.keys(toUpdate).length === 0){
+    res.status(400).json({message: "No changes provided"})
+ }
+
+ const updateTodoList = await Todos.findByIdAndUpdate(id, toUpdate, {new: true})
+    if(!updateTodoList){
+        return res.status(400).json({message: "TodoList not found"})
+    }
+ res.status(200).json({updateTodoList})
+})
+
+//Delete
+export const deleteTodoList = asyncHandler(async(req, res, next)=> {
+    const {id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({message: "Invalid Id"})
+    }
+
+    const todoList = await Todos.findByIdAndDelete(id).exec()
+    if(!todoList){
+        return res.status(400).json({message: "TodoList not found"})
+    }
+
+       res.sendStatus(204)
+})
+
+
+
+
+
--- backend_todoListDB/src/routers/todo.route.js
+++ backend_todoListDB/src/routers/todo.route.js
@@ -1,15 +1,17 @@
 import express from "express"
+import Todos from "../models/todos.models.js"
+import { createTodoList, deleteTodoList, getTodoList, getTodoListById, updateTodoList } from "../controlers/todo.controlers.js"
 
 const router = express.Router()
 
 
 //CRUD
-router.post('/', ()=> {}) //create
-router.get('/', ()=> {}) //Read
-router.get('/:id', ()=> {}) //Read
-router.put('/', ()=> {}) //update
-router.patch('/', ()=> {}) //update
-router.delete('/', ()=> {}) //Delete
+router.post('/', createTodoList ) //create
+router.get('/', getTodoList) //Read
+router.get('/:id',getTodoListById) //Read
+// router.put('/', ()=> {}) //update
+router.patch('/:id', updateTodoList) //update
+router.delete('/:id', deleteTodoList) //Delete
 
 
 export default router
